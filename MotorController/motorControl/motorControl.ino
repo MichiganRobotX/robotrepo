0c0bbda4b4f5c28007d0d5cfea4e99fc8dd1970d
James Coller <jcoller@umich.edu>
2018-06-20 11:04:45 -0400

    Re-mapped the float conversion
    Updated motor controller with the remapping function. Tested on vehicle on June 3.

--- MotorController/motorControl/motorControl.ino
+++ MotorController/motorControl/motorControl.ino
@@ -1,44 +1,75 @@
 // WAM-V Motor Controller
 
-#include <Servo.h>
-// #include <ros.h>
+#include <ros.h>
+#include <sensor_msgs/Joy.h>
+#include <std_msgs/String.h>
+#include <stdio.h>
 
-// Somehwere in here we need to add getting the controller info from 
-
-int outpin = 9; // PWM Output port 
-int pos = 0;    // variable to store the servo position
-int potpin = 1; // Analog In Location from the potentiometer 
+int pinL = 9; // PWM Output port for the left side
+int pinR = 10; // PWM Ouptut port for the right side
+//int potpin = 1; // Analog In Location from the potentiometer
 int speed;
+float powerL = 125;
+float powerR = 125;
+float posL;
+float posR;
+char buffer[50];
+
+std_msgs::String str_msg;
+ros::Publisher chatter("chatter", &str_msg);
+
+// Set the Joystick Function
+
+void joyuse ( const sensor_msgs::Joy& joy){
+
+  // Set the joystick values
+  posL = joy.axes[1];
+  posR = joy.axes[4];
+
+  // Remap Joystick
+  powerL = 127 + 127*posL;
+  powerR = 127 + 127*posR;
+
+  //powerL = map(posL, -1, 1, 0, 255); // Left Side
+  //powerR = map(posR, -1, 1, 0, 255); // Right Side
+
+  int speedL = static_cast<int>(powerL);
+  int speedR = static_cast<int>(powerR);
+
+  analogWrite(pinL, speedL); // tell left motor how fast
+  analogWrite(pinR, speedR); // tell right motor how fast
+
+  // say what you got:
+  sprintf( buffer, "The left motor speed is %f", posL);
+  str_msg.data = buffer;
+  chatter.publish( &str_msg );
+
+}
+
+// Setup ROS
+
+ros::NodeHandle nh;
+ros::Subscriber<sensor_msgs::Joy> sub("joy", &joyuse);
+
 
 void setup() {
-  
-  // put your setup code here, to run once:
-  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
-  // Request a motor speed from the serial window 
-  Serial.println("Please provide a motor speed between 0 and 255.");
+  // ROS Input
+  nh.initNode();
+  nh.subscribe(sub);
+  nh.advertise(chatter);
 }
 
 void loop() {
-  // Run motor only if you receive data 
-  if (Serial.available() > 0) {
-      // read the incoming byte:
-      speed = Serial.parseInt();
+  double random = 125;
+  // sprintf(buffer, "the main void motor speed is %d", random);
 
-      // say what you got:
-      Serial.print("Motor speed set to: ");
-      Serial.println(speed, DEC);
+  // Here Corina suggests asdding the while loop for Whiile (ros::ok()) and to put in a frequency, but we couldn't get that working the first time around.
+  // it may be related to the rosserial
 
-      if (speed > 255){
-        speed = 255;
-        Serial.println("Speed set too high, speed reset to 255.");
-      }
-
-      if (speed < 0){
-        speed = 0;
-        Serial.println("Speed cannot be less than 0, speed set to 0.");
-      }
-         
-         
-  analogWrite(outpin, speed); // tell servo to go to position in variable 'pos'   
-  }
+  nh.spinOnce();
+  str_msg.data = buffer;
+  chatter.publish( &str_msg );
+  delay(1000);
+  //nh.spinOnce();
+  //elay(1);
 }
