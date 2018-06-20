1a017cee168fe0f625edb2f48e86d6e45caac786
James Coller <jcoller@umich.edu>
2018-06-20 12:57:21 -0400

    Updated to include mapping for wired joystick

--- MotorController/joyStick_setup.sh
+++ MotorController/joyStick_setup.sh
@@ -3,11 +3,14 @@
 # Set the joystick permission
 sudo chmod a+rw /dev/input/js2
 
-# Mao the joystick in ros
+# Map the joystick in ros
+# for wireless joystick, use js2
+# for wired joystick, use js0
 rosparam set joy_node/dev "/dev/input/js2"
 
 # Start the Joystick Node
 rosrun joy joy_node
 
-# Open a new terminal and show the joystick 
-terminal -e rostopic echo joy
+# Open a new terminal and show the joystick
+# this doesn't work
+# terminal -e rostopic echo joy
