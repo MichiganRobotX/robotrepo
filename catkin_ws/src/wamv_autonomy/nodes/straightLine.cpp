ac5fa5fe07cc6a5a9ce92338b956ce1493afeea3
Sahib Dhanjal <dhanjalsahib@gmail.com>
2018-08-20 22:23:13 -0400

    PID modular

--- catkin_ws/src/wamv_autonomy/nodes/straightLine.cpp
+++ catkin_ws/src/wamv_autonomy/nodes/straightLine.cpp
@@ -2,15 +2,8 @@
 Node for WamV straight Line Task
 */
 
-#include <bits/stdc++.h>
 #include <ros/ros.h>
-#include <std_msgs/Int16.h>
-#include <nav_msgs/Odometry.h>
-#include <sensor_msgs/Imu.h>
-#include <sensor_msgs/Joy.h>
-#include <geometry_msgs/PoseWithCovariance.h>
-
-#define endl "\n"
+#include "PID.h"
 
 #define dt 1/30
 #define distance 10
@@ -24,80 +17,78 @@ Subscriber odom_sub;
 Subscriber joy_sub;
 
 // Global Variables
-bool activate = false;
+bool activate = true;
 bool flag = true;
-float prev_error = 0;
 float X, Y, origin_X, origin_Y;
-float pterm, dterm, iterm;
-float iMax = 1, iMin = -1, dMax = 1, dMin = -1;
 
-// Function Definitions
-float PID(float kp, float kd, float ki, float error) {
-	pterm = kp*error;
-	
-	iterm = iterm + ki*error;
-	if(iterm>iMax)
-		iterm = iMax;
-	if(iterm<iMin)
-		iterm = iMin;
-
-	dterm = kd*(error - prev_error)/dt;
-	if(dterm>dMax)
-		dterm = dMax;
-	if(dterm<dMin)
-		dterm = dMin;
-
-	return (pterm + dterm + iterm);
-}
-
-void joy_callback(const sensor_msgs::Joy &msg) {
-	if(msg.buttons[0]==1)
-		activate = true;
-}
-
-void gps_callback(const nav_msgs::Odometry &msg) {
-	X = msg.pose.pose.position.x;
-	Y = msg.pose.pose.position.y;
-	ROS_INFO_STREAM("x-pos: "<<X<<" | y-pos: "<<Y);
-}
+// Set the speeds for each 
+int backwardSpeed = 20;
+int zeroSpeed = 127;
+int forwardSpeed = 220;
 
 void publisher() {
 	if(activate){
 		std_msgs::Int16 right_msg;
 		std_msgs::Int16 left_msg;
+
+		// Initialize PID here with parameters 
+		PID yaw_pid (0.1, 0.1, 0.1, dt);
+		yaw_pid.PID_SetI(10, -10);
+		yaw_pid.PID_SetD(10, -10);
+		yaw_pid.PID_SetOut(100, -100);
+
 		
-		// 
 		if(flag) {
 			origin_X = X;
 			origin_Y = Y;
 			flag = false;
+			ROS_WARN_STREAM("Origin: "<<origin_X<<", "<<origin_Y);
 		}
 		else {
 			// set reference frame to X
-			X = X - origin_X;
-			Y = Y - origin_Y;
+			X -= origin_X;
+			Y -= origin_Y;
 
 			float errorY = 0 - Y;
-			float outY = PID(0.1, 0.1, 0.1, errorY);
+
+			float outY = yaw_pid.PID_Compute(errorY);
 			
 			// Bang Bang Control. Keep on applying motor commands 
 			// till you reach the target distance. outY makes minor 
 			// adjustments to make sure the boat is straight
-			right_msg.data = 250 - outY;
-			left_msg.data = 250 + outY;
+			right_msg.data = forwardSpeed - outY;
+			left_msg.data = forwardSpeed + outY;
 
 			// set control commands to 0 if target distance achieved
 			if(X>=distance) {
-				right_msg.data = 127;
-				left_msg.data = 127; 
+				right_msg.data = zeroSpeed;
+				left_msg.data = zeroSpeed;
+				ROS_WARN_STREAM("Reached the Target!");
 			}
 		}
 		
 		right_pub.publish(right_msg);
 		left_pub.publish(left_msg);
-		ROS_INFO_STREAM("left_msg: "<<left_msg.data<<" | right_msg: "<<right_msg.data);
+		ROS_INFO_STREAM("X: "<<X<<" | Y: "<<Y<<" | left_msg: "<<left_msg.data<<" | right_msg: "<<right_msg.data);
 	}
 }
+
+void joy_callback(const sensor_msgs::Joy &msg) {
+	// open activate if button 1 pressed
+	if(msg.buttons[0]==1)
+		activate = true;
+	
+	// close activate if button 2 pressed
+	if(msg.buttons[1]==1)
+		activate = false;
+}
+
+void gps_callback(const nav_msgs::Odometry &msg) {
+	X = msg.pose.pose.position.x;
+	Y = msg.pose.pose.position.y;
+	publisher();
+}
+
 
 int main(int argc, char **argv) {
 	init(argc, argv, "straightLine");
