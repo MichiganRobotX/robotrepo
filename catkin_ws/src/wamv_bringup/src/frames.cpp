673e91dde01666012e1fe85de890f4e3d8a7fecf
Sahib Dhanjal <dhanjalsahib@gmail.com>
2018-09-23 20:46:13 -0400

    tf update & gmapping added

--- catkin_ws/src/wamv_bringup/src/frames.cpp
+++ catkin_ws/src/wamv_bringup/src/frames.cpp
@@ -1,61 +1,54 @@
-/*
-This node will be broadcasting the transforms for
-different frames on the wamv, namely:
-(i)     base_link   - the centroid of the robot
-(ii)    gps         - the position of the gps wrt the base_link
-(iii)   velodyne    - the position of the velodyne wrt the base_link
-(iv)    camera      - the position of the camera wrt the base_link
-(v)     imu         - the position of the imu wrt the base_link
-
-*/
-
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+
+void handleFrame(const nav_msgs::Odometry &msg) {
+    static tf::TransformBroadcaster br;
+    tf::Transform transform;
+    float qx = msg.pose.pose.position.x;
+    float qy = msg.pose.pose.position.y;
+
+    for(int i=1; i<=5; i++) {
+        // world to map
+        if(i==1) {
+            transform.setOrigin(tf::Vector3(0, 0, 0));
+            transform.setRotation(tf::Quaternion(0, 0, 0, 1));
+            br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "map"));
+        }
+        // map to base_link
+        if(i==2) {
+            transform.setOrigin(tf::Vector3(qx, qy, 0.5));
+            transform.setRotation(tf::Quaternion(0, 0, 0, 1));
+            br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "map", "base_link"));
+        }
+        // base_link to gps
+        if(i==3) {
+            transform.setOrigin(tf::Vector3(0, 0, 0));
+            transform.setRotation(tf::Quaternion(0, 0, 0, 1));
+            br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "gps"));
+        }
+        // velodyne to base_link
+        if(i==4) {
+            transform.setOrigin(tf::Vector3(2.4, 0, 1.8));
+            transform.setRotation(tf::Quaternion(0, 0, 0, 1));
+            br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "velodyne"));
+        }
+        // base_link to odom
+        if(i==5) {
+            transform.setOrigin(tf::Vector3(0, 0, 0));
+            transform.setRotation(tf::Quaternion(0, 0, 0, 1));
+            br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "odom"));
+        }
+    }
+}
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "wamv_tf_publisher");
     ros::NodeHandle nh;
-
-    ros::Rate rate(100);
-
-    tf::TransformBroadcaster br;
-    tf::Transform tf_base_gps;
-    tf::Transform tf_base_imu;
-    tf::Transform tf_base_cam;
-    tf::Transform tf_base_vel;
-    tf::Transform tf_base_map;
-    tf::Transform tf_gps_velo;
-
-
-    while(nh.ok()){
-        tf_base_map.setOrigin(tf::Vector3(0.0, 0.0, -0.5));
-        tf_base_map.setRotation(tf::Quaternion(0, 0, 0, 1));
-
-        tf_base_gps.setOrigin(tf::Vector3(2.4, 0.0, 1.8));
-        tf_base_gps.setRotation(tf::Quaternion(0, 0, 0, 1));
-
-        tf_base_imu.setOrigin(tf::Vector3(2.4, 0.0, 1.8));
-        tf_base_imu.setRotation(tf::Quaternion(0, 0, 0, 1));
-
-        tf_base_vel.setOrigin(tf::Vector3(2.4, 0.0, 1.6));
-        tf_base_vel.setRotation(tf::Quaternion(0, 0, 0, 1));
-
-        tf_base_cam.setOrigin(tf::Vector3(2.4, 0.0, 1.5));
-        tf_base_cam.setRotation(tf::Quaternion(0, 0, 0, 1));
-
-        tf_gps_velo.setOrigin(tf::Vector3(0.0, 0.0, 0.2));
-        tf_gps_velo.setRotation(tf::Quaternion(0, 0, 0, 1));
-
-
-        br.sendTransform(tf::StampedTransform(tf_base_map, ros::Time::now(), "base_link", "map"));
-        br.sendTransform(tf::StampedTransform(tf_base_vel, ros::Time::now(), "base_link", "velodyne"));
-        br.sendTransform(tf::StampedTransform(tf_base_cam, ros::Time::now(), "base_link", "camera"));
-        br.sendTransform(tf::StampedTransform(tf_base_gps, ros::Time::now(), "base_link", "gps"));
-        br.sendTransform(tf::StampedTransform(tf_base_imu, ros::Time::now(), "base_link", "imu"));
-        br.sendTransform(tf::StampedTransform(tf_gps_velo, ros::Time::now(), "gps", "velodyne"));
-
-        ROS_WARN("Publishing Transform Frames...");
-
+    ros::Subscriber odom_sub = nh.subscribe("/gps/odom", 100000, &handleFrame);
+    ros::Rate rate(10);
+    while(ros::ok()) {
+        ros::spinOnce();
         rate.sleep();
     }
     return 0;
